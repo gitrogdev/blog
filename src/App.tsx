0ebf1cdd08b217318a1d83038447cad86a40b5d6
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-07 15:26:58 -0500

    Parse blog posts from file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { blogPosts } from './blog/blogPosts';
 import About from './components/About/About';
 import Nav from './components/Nav/Nav';
 import PostGallery from './components/PostGallery/PostGallery';
@@ -6,23 +7,6 @@ export default function App() {
 	return (<>
 		<Nav />
 		<About />
-		<PostGallery blogPosts={[{
-			date: '2025-08-19',
-			thumbnail: 'https://photos.smugmug.com/photos/i-wxQjFJ9/0/LzqJZpzkwtGr93DBm88CXLqV5nBC4q227WfrBP696/X3/i-wxQjFJ9-X3.jpg',
-			title: 'A Quick Trip to Beograd',
-			daterange: 'August 19th, 2025 - August 21st, 2025'
-		},
-		{
-			date: '2025-08-19',
-			thumbnail: 'https://photos.smugmug.com/photos/i-wxQjFJ9/0/LzqJZpzkwtGr93DBm88CXLqV5nBC4q227WfrBP696/X3/i-wxQjFJ9-X3.jpg',
-			title: 'A Quick Trip to Beograd',
-			daterange: 'August 19th, 2025 - August 21st, 2025'
-		},
-		{
-			date: '2025-08-19',
-			thumbnail: 'https://photos.smugmug.com/photos/i-wxQjFJ9/0/LzqJZpzkwtGr93DBm88CXLqV5nBC4q227WfrBP696/X3/i-wxQjFJ9-X3.jpg',
-			title: 'A Quick Trip to Beograd',
-			daterange: 'August 19th, 2025 - August 21st, 2025'
-		}]}/>
+		<PostGallery blogPosts={blogPosts}/>
 	</>);
 };
--- src/blog/blogPosts.ts
+++ src/blog/blogPosts.ts
@@ -1,3 +1,5 @@
+const postFiles = import.meta.glob('./posts/*.md', { eager: true, as: 'raw' });
+
 /**
  * Represents a blog post for the site.
  */
@@ -12,5 +14,23 @@ export interface BlogPost {
 	title: string,
 
 	/** The human-readable date or date range of the post. */
-	daterange: string
+	daterange: string,
+
+	/** The content of the blog post, as markdown. */
+	content?: string
 }
+
+function parsePost(contents: string): BlogPost {
+	const lines = contents.split('\n');
+	return {
+		date: lines[0].trim(),
+		thumbnail: lines[1].trim(),
+		title: lines[2].trim(),
+		daterange: lines[3].trim(),
+		content: lines.slice(4).join('\n').trim()
+	}
+}
+
+export const blogPosts: BlogPost[] = Object.values(postFiles).map(
+	(contents) => parsePost(contents)
+).sort((a, b) => b.date.localeCompare(a.date));
