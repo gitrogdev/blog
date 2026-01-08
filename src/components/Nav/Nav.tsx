965a25df8d4d1b66f3976c71fcab13a4a4e456af
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-07 20:45:55 -0500

    Add a link back to the home page

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,15 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 import Links from '../Links/Links';
+import "./Nav.css";
 
 export default function Nav() {
 	return (
-		<AppBar position="static">
+		<AppBar position="static" className="nav-bar">
 			<Toolbar>
 				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					gitblog
+					<Link to="">gitblog</Link>
 				</Typography>
 				<Links/>
 			</Toolbar>
