f688c225d7f1c8fd5e8c5481ac277ed0a640e2b5
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-07 20:43:01 -0500

    Jump to top when a link is used

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import About from "./pages/About/About";
 import BlogPost from "./pages/BlogPost/BlogPost";
+import JumpToTop from "./components/JumpToTop/JumpToTop";
 
 export default function App() {
 	return (<HashRouter>
+		<JumpToTop/>
 		<Routes>
 			<Route element={<Layout/>}>
 				<Route path="/" element={<About/>}/>
