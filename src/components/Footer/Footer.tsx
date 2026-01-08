db06932851c8af4a9d2e7026037f9b20e8f41193
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-08 17:09:03 -0500

    Add Material UI to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,9 +23,15 @@ export default function Footer() {
 				underline="hover"
 			>
 				TypeScript
-			</Link>, and{" "}
+			</Link>,{" "}
 			<Link href="https://react.dev/" target="_blank" underline="hover">
 				React.js
+			</Link>, and {" "}
+			<Link href="https://mui.com/material-ui/"
+				target="_blank"
+				underline="hover"
+			>
+				Material UI
 			</Link>. Hosted on{" "}
 			<Link href="https://pages.github.com/"
 				target="_blank"
