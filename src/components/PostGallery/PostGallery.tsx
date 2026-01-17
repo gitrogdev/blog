86d4132e48f1ac9e2a671dbd0fa02daab7766a7d
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-18 00:49:46 +0100

    Make tabs for different categories

--- src/components/PostGallery/PostGallery.tsx
+++ src/components/PostGallery/PostGallery.tsx
@@ -1,11 +1,13 @@
+import { useState, type SyntheticEvent } from "react";
 import {
+	Box,
 	CircularProgress,
 	ImageList,
 	ImageListItem,
 	ImageListItemBar,
-	ListSubheader
+	Tab,
 } from "@mui/material";
-import { useState } from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
 import type { BlogPost } from "../../blog/blogPosts";
 import './PostGallery.css';
@@ -30,52 +32,60 @@ export default function PostGallery(props: PostGalleryProps) {
 			{} as Record<string, boolean>
 		)
 	);
+	const [tab, setTab] = useState('1');
 
 	const handleLoad = (thumbnail: string) => {
 		setLoading((prev) => ({ ...prev, [thumbnail]: false }));
 	};
 
-	return (
-		<ImageList>
-			<ImageListItem key="Subheader" cols={2}>
-				<ListSubheader component="div">Blog Posts</ListSubheader>
-			</ImageListItem>
+	const handleTab = (_event: SyntheticEvent, index: string) => setTab(index);
 
-			{props.blogPosts.map((post) => (<Link to={`blog/${post.slug}`}>
-				<ImageListItem
-					key={post.thumbnail}
-					className="post-preview"
-				>
-					{loading[post.thumbnail] && (
-						<div
-							style={{
+	return (<TabContext value={tab}>
+		<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+			<TabList onChange={handleTab}>
+				<Tab label="Travel" value="1" />
+				<Tab label="Music" value="2" />
+			</TabList>
+		</Box>
+		<TabPanel value="1">
+			<ImageList>
+				{props.blogPosts.map((post) => (<Link to={`blog/${post.slug}`}>
+					<ImageListItem
+						key={post.thumbnail}
+						className="post-preview"
+					>
+						{loading[post.thumbnail] && (
+							<div style={{
 								position: "absolute",
 								top: "50%",
 								left: "50%",
 								transform: "translate(-50%, -50%)",
-							}}
-						>
-							<CircularProgress />
-						</div>
-					)}
+							}} >
+								<CircularProgress />
+							</div>
+						)}
 
-					<img
-						src={post.thumbnail}
-						alt={post.title}
-						loading="lazy"
-						onLoad={() => handleLoad(post.thumbnail)}
-						style={
-							loading[post.thumbnail]
-							? { visibility: "hidden" } : {}
-						}
-					/>
+						<img
+							src={post.thumbnail}
+							alt={post.title}
+							loading="lazy"
+							onLoad={() => handleLoad(post.thumbnail)}
+							style={
+								loading[post.thumbnail]
+									? { visibility: "hidden" } : {}
+							}
+						/>
 
-					<ImageListItemBar 
-						title={post.title}
-						subtitle={post.daterange}
-					/>
-				</ImageListItem>
-			</Link>))}
-		</ImageList>
-	);
+						<ImageListItemBar
+							title={post.title}
+							subtitle={post.daterange}
+						/>
+					</ImageListItem>
+				</Link>))}
+			</ImageList>
+		</TabPanel>
+		<TabPanel value="2">
+			music
+		</TabPanel>
+	</TabContext>);
 }
